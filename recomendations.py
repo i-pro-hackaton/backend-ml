3b4cd582f7031c8059373604a330df32df2d29a8
huscker <egostalker03@mail.ru>
2022-06-12 09:54:26 +0300

    Changed neural network architecture

--- recomendations.py
+++ recomendations.py
@@ -1,18 +1,19 @@
 from tensorflow import keras
-from tensorflow.keras.layers import Input, Embedding, Dot, Reshape, Dense
+from tensorflow.keras.layers import Input, Embedding, Dot, Reshape, Dense, LSTM
 from tensorflow.keras.models import Model
 
-bought_products = Input(name = 'bought products', shape = [1])
-skipped_products = Input(name = 'skipped products', shape = [1])
-
-bought_embedding = Embedding(name = 'bought_embedding',
+clicked_products = Input(name = 'clicked products', shape = [1])
+other_criteries = Input(name = 'otner criteries', shape = [10])
+criteries = Dense(10, activation='softmax')(other_criteries)
+clicked_embedding = Embedding(name = 'clicked_embedding',
                            input_dim = 1000,
-                           output_dim = 10)(bought_products)
-skipped_embedding = Embedding(name = 'skipped_embedding',
-                           input_dim = 1000,
-                           output_dim = 10)(skipped_products)
-merged = Dot(name = 'dot_product', normalize = True, axes = 2)([bought_embedding, skipped_embedding])
+                           output_dim = 10)(clicked_products)
+x = LSTM(units = 50, return_sequences = True)(clicked_embedding)
+x = LSTM(units = 20, return_sequences = True)(x)
+x = LSTM(units = 10, return_sequences = True)(x)
+x = Reshape((10,))(x)
+merged = Dot(name = 'dot_product', normalize = True, axes = 1)([x, criteries])
 merged = Reshape(target_shape = [1])(merged)
 out = Dense(1, activation = 'sigmoid')(merged)
-model = Model(inputs = [bought_products, skipped_products], outputs = out)
+model = Model(inputs = [clicked_products, other_criteries], outputs = out)
 model.compile(optimizer = 'Adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
